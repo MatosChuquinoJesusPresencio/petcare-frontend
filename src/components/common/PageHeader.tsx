3dd34f00477f871ee13a7b488f1642551f7d708f
EdhuMS <edhums10@gmail.com>
2026-06-08 20:24:47 -0500

    feat: redisenar PageHeader

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -9,17 +9,15 @@ interface PageHeaderProps {
 
 export default function PageHeader({ icon, title, description, children }: PageHeaderProps) {
   return (
-    <section className="card mb-3">
-      <div className="card-body d-flex flex-row justify-content-between align-items-center">
-        <div>
-          <div className="d-flex gap-2 align-items-center">
-            {icon && <i className={`bi ${icon} fs-2`}></i>}
-            <h1 className="mb-0">{title}</h1>
-          </div>
-          {description && <p className="text-muted mb-0 mt-1">{description}</p>}
-        </div>
-        {children && <div className="d-flex gap-2 align-items-center">{children}</div>}
+    <div className="encabezado-pagina">
+      <div className="encabezado-pagina-info">
+        <h1 className="encabezado-pagina-titulo">
+          {icon && <i className={`bi ${icon}`}></i>}
+          {title}
+        </h1>
+        {description && <p className="encabezado-pagina-descripcion">{description}</p>}
       </div>
-    </section>
+      {children && <div className="encabezado-pagina-acciones">{children}</div>}
+    </div>
   );
 }
